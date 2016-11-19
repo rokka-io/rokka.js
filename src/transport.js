abd7a226346933de64d5d50caf8ddc69f87759ea
Christian Stocker <me@chregu.tv>
2016-11-19 22:51:19 +0100

    automatically retry 10 times when the server sends a 429

--- src/transport.js
+++ src/transport.js
@@ -1,3 +1,39 @@
 import request from 'request-promise';
+import promiseRetry from 'promise-retry';
+import isDefined from 'check-defined';
+import fs from 'fs';
 
-export default request;
+
+// When the API sends a 429 back, retry it for 10 times
+// This code is mainly copied from
+// https://github.com/CanTireInnovations/request-promise-retry/blob/master/index.js
+
+var retryRequest = function( promiseRequest ) {
+  return function( req ) {
+
+    return promiseRetry(
+      function( retry ) {
+
+        return promiseRequest( req )
+          .catch( function( err ) {
+            if (err.statusCode === 429) {
+              //if the request has filedata, reread stream
+              if (isDefined(req, 'formData.filedata.value.path')) {
+                req.formData.filedata.value = fs.createReadStream(req.formData.filedata.value.path);
+              }
+              retry(err);
+            }
+            throw err ;
+          } );
+      },
+      {
+        retries: 10,
+        minTimeout: 1000,
+        maxTimeout: 10000,
+        randomize: true
+      }
+    );
+  };
+};
+
+export default retryRequest(request);
