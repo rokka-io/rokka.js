cbf7c0652e8979dd3059527a7749310d60d73603
Michael Weibel <michael.weibel@liip.ch>
2017-05-02 11:13:15 +0200

    Specify organization in user create test

--- test/apis/users.test.js
+++ test/apis/users.test.js
@@ -9,12 +9,12 @@ import rka from '../../src'
 test('users.create', t => {
   const rokka = rka({ apiKey: 'APIKEY' })
 
-  rokka.users.create('user@example.org')
+  rokka.users.create('user@example.org', 'user-at-example-organization')
 
   const expectedArgs = {
     method: 'POST',
     uri: 'https://api.rokka.io/users',
-    body: { email: 'user@example.org' }
+    body: { email: 'user@example.org', organization: 'user-at-example-organization' }
   }
 
   td.verify(requestStub(td.matchers.contains(expectedArgs), td.matchers.anything()))
