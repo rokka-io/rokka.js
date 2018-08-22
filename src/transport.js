40f69e41402c990d6729cdf13978615d425a3986
Christian Stocker <me@chregu.tv>
2018-08-22 12:59:38 +0200

    retry also on 502, 503 and 504
    we do that already in the PHP client. Makes sense from the server point of view as well

--- src/transport.js
+++ src/transport.js
@@ -2,6 +2,7 @@ import request from 'request-promise'
 import promiseRetry from 'promise-retry'
 
 // When the API sends a 429 back, retry it for 10 times
+// We also retry on server errors 502, 503 and 504
 // This code is mainly copied from
 // https://github.com/CanTireInnovations/request-promise-retry/blob/master/index.js
 
@@ -9,7 +10,7 @@ const retryRequest = (promiseRequest) =>
   (req, options) => promiseRetry(
     retry => promiseRequest(req)
       .catch(err => {
-        if (err.statusCode !== 429) {
+        if (err.statusCode !== 429 && err.statusCode !== 502 && err.statusCode !== 503 && err.statusCode !== 504) {
           throw err
         }
         retry(err)
