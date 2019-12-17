c3906fc0e845820bb4c865f7185c0595a9154bae
Christian Stocker <me@chregu.tv>
2019-12-17 23:08:25 +0100

    update to latest versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import { terser } from 'rollup-plugin-terser'
-import resolve from 'rollup-plugin-node-resolve'
+import resolve from '@rollup/plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 export default [
