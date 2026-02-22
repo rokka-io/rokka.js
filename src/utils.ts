dbad07b91d8643bf698fae712b0e425481f04e17
Christian Stocker <me@chregu.tv>
2026-02-22 18:41:09 +0100

    build: upgrade jwt-decode from v3 to v4
    Switch from default import to named export (jwtDecode).

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import jwtDecode from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 import { ApiTokenGetCallback, ApiTokenPayload } from './apis/user'
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
